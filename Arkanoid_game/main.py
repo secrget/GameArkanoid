a4453cecd7337b91d4b423cc8b212fcb0e48616b
Yegor <mamitkoegorb@gmail.com>
2023-03-20 18:07:32 +0200

    new project commit

---
+++ Arkanoid_game/main.py
@@ -0,0 +1,10 @@
+import pygame
+from classes.Game import Game
+
+
+
+if __name__ == "__main__":
+    game = Game([800, 600])
+    game.run()  # thats all
+
+
---
+++ Arkanoid_game/test/test_brick.py
@@ -0,0 +1,28 @@
+import pytest
+
+from classes.Brick import Brick
+from classes.Ball import Ball
+
+
+def test_brick_creation():
+    brick = Brick([0, 0], 50, 20)
+    assert brick.position == [0, 0]
+    assert brick.width == 50
+    assert brick.height == 20
+    assert brick.status == "unharmed"
+
+
+def test_brick_destroy():
+    brick = Brick([0, 0], 50, 20)
+    brick.destroy()
+    assert brick.status == "destroyed"
+
+
+def test_brick_collision():
+    brick = Brick([0, 0], 50, 20)
+    ball = Ball([0, 0], [5, 5])
+    assert brick.check_collision(ball) == True
+
+
+if __name__ == "__main__":
+    pytest.main()
